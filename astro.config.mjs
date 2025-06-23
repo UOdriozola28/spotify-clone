7a1d18d62de067d887c56aa571758c21c90fa8a1
Walin Paredes <paolitomaldonadofarje@gmail.com>
2025-06-23 17:47:06 -0500

    Configure Astro with Vite and Svelte integration; add initial Svelte config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
+
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [
+      tailwindcss(),
+      react()
+    ]
+  },
+  output: 'server',
+  integrations: [svelte(), react()]
+});
